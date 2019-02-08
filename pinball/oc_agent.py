6b9239093a809b1ec5370a5e17e4c7affef8ae77
takato86 <okudo@nii.ac.jp>
2019-02-08 23:16:12 +0900

    [feat] add option critic for pinball.

---
+++ pinball/oc_agent.py
@@ -0,0 +1,189 @@
+import argparse
+import numpy as np
+import sys
+import gym
+from gym import wrappers, logger
+import gym_pinball
+import pdb
+
+class OptionCriticAgent(object):
+    """
+    価値関数近似：Fourier basis, https://scholarworks.umass.edu/cgi/viewcontent.cgi?referer=https://www.google.com/&httpsredir=1&article=1100&context=cs_faculty_pubs
+
+    """
+    def __init__(self, action_space, observation_space):
+        self.action_space = action_space
+        self.basis_order = 3
+        self.shape_state = observation_space.shape
+        self.options = [Option(action_space.n, observation_space.shape[0]) for i in range(4)]
+        self.w_q = np.random.rand(len(self.options), action_space.n, self.basis_order) # the number of orders
+        self.c_phi = np.random.randint(0, self.basis_order + 1, (len(self.options), action_space.n, self.basis_order, observation_space.shape[0]))
+        self.c_phi = self.c_phi.astype(np.float64)
+        self.q_u = np.random.rand(len(self.options), action_space.n)
+        self.gamma = 0.01
+        self.lr_wq = 0.01
+        self.lr_cphi = 0.01
+    def act(self, observation, o):
+        option = self.options[o]
+        q_u_list = self._get_q_u_list(observation, o)
+        action = np.argmax(option.get_intra_option_dist(q_u_list))
+        return action
+    
+    def update(self, pre_obs, a, obs, r, done, o):
+        q_u_list = self._get_q_u_list(pre_obs, o)
+        td_error = r - q_u_list[a]
+        option = self.options[o]
+        if not done:
+            term_prob = option.get_terminate(obs)
+            q_omega_list = self._get_q_omega_list(obs) #戦犯
+            td_error += self.gamma * ((1 - term_prob) * q_omega_list[o] + term_prob * np.max(q_omega_list))
+        self._update_w_q(td_error, a, o, pre_obs)
+        self._update_c_phi(td_error, a, o, pre_obs)
+        q_omega = self._get_q_omega(obs, o)
+        v_omega = self._get_v_omega(obs)
+        option.update(a, obs, q_u_list, q_omega, v_omega)
+
+    def _update_w_q(self, td_error, a, o, obs):
+        delta_list = []
+        for c_phi_a in self.c_phi[o][a]:
+            delta = np.cos(np.pi * np.dot(c_phi_a, obs))
+            delta_list.append(self.lr_wq * delta * td_error)
+        update_list = np.array(delta_list)
+        self.w_q[o][a] += update_list
+    
+    def _update_c_phi(self, td_error, a, o, obs):
+        delta_list = []
+        for i in range(self.basis_order):
+            c_delta_list = []
+            basis_delta = self.w_q[o][a][i] * np.sin(np.pi * np.dot(self.c_phi[o][a][i], obs)) * np.pi
+            for j in range(len(obs)):
+                c_delta = self.lr_cphi * basis_delta * obs[j] * td_error
+                c_delta_list.append(c_delta)
+            delta_list.append(c_delta_list)
+        update_list = np.array(delta_list)
+        #if self.c_phi[o].shape[0] == update_list.shape[0] and self.c_phi[o].shape[1] == update_list.shape[1]:
+        self.c_phi[o][a] += update_list
+
+    def _get_q_u(self, obs, o, a):
+        """
+        Fourier basis of order 3
+        """
+        phis = np.array([self._get_phi(obs, o, a, i) for i in range(self.basis_order)])
+        appr_q = np.sum(np.dot(self.w_q[o], phis))
+        return appr_q
+
+    def _get_q_u_list(self, obs, o):
+        return np.array([self._get_q_u(obs, o, a) for a in range(self.action_space.n)])
+
+    def _get_q_omega(self, obs, o):
+        option = self.options[o]
+        q_u_list = self._get_q_u_list(obs, o)
+        policy = option.get_intra_option_dist(q_u_list)
+        import pdb; pdb.set_trace()
+        return np.dot(policy, q_u_list)
+
+    def _get_q_omega_list(self, obs):
+        return [self._get_q_omega(obs, o) for o in range(len(self.options))]
+
+    def _get_v_omega(self, obs):
+        """
+        Policy over options is decided determistically.
+        """
+        q_omega_list = self._get_q_omega_list(obs)
+        return np.max(q_omega_list)
+
+    def _get_phi(self, obs, o, a, i):
+        """
+        Basis function: 基底関数
+        """
+        c = self.c_phi[o][a][i]
+        x = np.array(obs)
+        in_tri = np.pi * np.dot(c, x)
+        return np.cos(in_tri)
+
+    def get_option(self, obs):
+        q_omega_list = self._get_q_omega_list(obs)
+        return np.argmax(q_omega_list)
+        
+class Option(object):
+    def __init__(self, n_actions, n_obs):
+        self.n_actions = n_actions
+        self.theta = np.random.rand(1)
+        self.vartheta = np.random.rand(n_obs)
+        self.lr_theta = 0.01
+        self.lr_vartheta = 0.01
+
+    def update(self, a, obs, q_u_list, q_omega, v_omega):
+        self._update_theta(a, obs, q_u_list)
+        self._update_vartheta(obs, q_omega, v_omega)
+    
+    def _update_theta(self, a, obs, q_u_list):
+        """
+        intra option policy gradient theorem
+        """
+        delta = -self.theta**-2*(q_u_list[a]+ np.sum(q_u_list))*q_u_list[a]
+        self.theta += self.lr_theta * delta
+    
+    def _update_vartheta(self, obs, q_omega, v_omega):
+        """
+        termination function gradient theorem
+        """
+        advantage = q_omega - v_omega
+        beta = self.get_terminate(obs)
+        for i, s in enumerate(obs):
+            self.vartheta[i] -= self.lr_vartheta * s * beta * (1 - beta)
+
+    def get_terminate(self, obs):
+        """
+        linear-sigmoid functions
+        """
+        linear_sum = np.dot(self.vartheta, obs)
+        return 1/(1 + np.exp(linear_sum))
+
+    def get_intra_option_dist(self, q_u_list):
+        """
+        Boltzmann policies
+        """
+        energy = q_u_list/self.theta
+        numerator = np.exp(energy)
+        denominator = np.sum(numerator)
+        return numerator/denominator
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=None)
+    parser.add_argument('env_id', nargs='?', default='PinBall-v0', help='Select the environment to run')
+    parser.add_argument('--debug', action='store_true')
+    args = parser.parse_args()
+    env = gym.make(args.env_id)
+
+    outdir = '/tmp/random-agent-results'
+    env = wrappers.Monitor(env, directory=outdir, force=True)
+    env.seed(0)
+    agent = OptionCriticAgent(env.action_space, env.observation_space)
+    episode_count = 100
+    reward = 0
+    done = False
+    try:
+        for i in range(episode_count):
+            print("episode: {}".format(i))
+            ob = env.reset()
+            option = agent.get_option(ob)
+            while True:
+                env.render()
+                action = agent.act(ob, option)
+                pre_obs = ob
+                ob, reward, done, _ = env.step(action)
+                if args.debug:
+                    agent.update(pre_obs, action, ob, reward, done, option)
+                    import pdb; pdb.set_trace()
+                    exit()
+                if done:
+                    break
+                # Note there's no env.render() here. But the environment still can open window and
+                # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
+                # Video is not recorded every episode, see capped_cubic_video_schedule for details.
+
+        # Close the env and write monitor result info to disk
+    except KeyboardInterrupt:
+        pass
+    env.close()
